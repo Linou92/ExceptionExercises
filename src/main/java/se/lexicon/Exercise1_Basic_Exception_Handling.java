598d78449e1072e34c0e4ef499851cf55b00dce2
Lina Abu Hijleh <lina_abuhijleh@hotmail.com>
2026-08-10 12:56:29 +0200

    Exercise1

--- src/main/java/se/lexicon/Exercise1_Basic_Exception_Handling.java
+++ src/main/java/se/lexicon/Exercise1_Basic_Exception_Handling.java
@@ -1,4 +1,16 @@
 package se.lexicon;
 
 public class Exercise1_Basic_Exception_Handling {
+
+    void Divide_By_Zero(){
+        int firstNumber = Integer.parseInt(IO.readln("Enter the first interger: "));
+        int secondNumber = Integer.parseInt(IO.readln("Enter the first interger: "));
+
+        try {
+            int result = firstNumber / secondNumber;
+            IO.println("The result is: " + result);
+        } catch (ArithmeticException e){
+            IO.println("❌ You cannot divide by zero: " + e.getMessage());
+        }
+    }
 }
